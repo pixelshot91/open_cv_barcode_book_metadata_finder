27333a8bff13550f9a71ba146fad7c3cae0ccd96
Julien Gautier <julien.gautier@mobile-devices.fr>
2023-03-01 19:26:34 +0100

    Update compile_main.sh

--- modules/barcode/compile_main.sh
+++ modules/barcode/compile_main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 clang++ -Ofast -iquote ../include -I /usr/local/include/opencv4/ -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -lopencv_dnn book_metadata_finder/main.cpp src/*.cpp src/decoder/*.cpp src/decoder/common/*.cpp  src/detector/*.cpp -o book_barcode_detect.release.out
-# clang++ -g -iquote ../include -I /usr/local/include/opencv4/ -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -lopencv_dnn src/*.cpp src/decoder/*.cpp src/decoder/common/*.cpp  src/detector/*.cpp
+#  clang++ -g -iquote ../include -I /usr/local/include/opencv4/ -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -lopencv_dnn book_metadata_finder/main.cpp src/*.cpp src/decoder/*.cpp src/decoder/common/*.cpp  src/detector/*.cpp -o book_barcode_detect.debug.out
